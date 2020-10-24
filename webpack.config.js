27147a481f7bbbf054225abc98d2d64c0a2e5816
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-24 23:06:32 +0200

    Enable Sass

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ Encore
     .enableVueLoader()
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
