928963b7aabeb77aa98c6b2c4e4a76a6c59b2601
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-11-07 10:12:08 +0100

    Fix indentation

--- assets/js/components/layout/TheHeader.vue
+++ assets/js/components/layout/TheHeader.vue
@@ -3,51 +3,51 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <span class="navbar-brand h1">Gift</span>
-      <ul
-        v-if="user"
-        class="navbar-nav mr-auto"
-      >
-        <li class="nav-item">
-          <a
-            :class="[activeClass('people'), 'nav-link']"
-            href="/peoples"
-          >Peoples</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li
+        <ul
           v-if="user"
-          class="nav-item dropdown"
+          class="navbar-nav mr-auto"
         >
-          <a
-            id="navbar-user-dropdown"
-            class="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >{{ user.email }}</a>
-          <div
-            class="dropdown-menu"
-            aria-labelledby="navbar-user-dropdown"
+          <li class="nav-item">
+            <a
+              :class="[activeClass('people'), 'nav-link']"
+              href="/peoples"
+            >Peoples</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li
+            v-if="user"
+            class="nav-item dropdown"
           >
             <a
-              href="/api/logout"
-              class="dropdown-item"
-            >Log out</a>
-          </div>
-        </li>
-        <li
-          v-else
-          class="nav-item active"
-        >
-          <a
-            class="btn btn-primary"
-            href="/login"
-          >Login</a>
-        </li>
-      </ul>
+              id="navbar-user-dropdown"
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >{{ user.email }}</a>
+            <div
+              class="dropdown-menu"
+              aria-labelledby="navbar-user-dropdown"
+            >
+              <a
+                href="/api/logout"
+                class="dropdown-item"
+              >Log out</a>
+            </div>
+          </li>
+          <li
+            v-else
+            class="nav-item active"
+          >
+            <a
+              class="btn btn-primary"
+              href="/login"
+            >Login</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
