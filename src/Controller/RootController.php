326bfd1bad2220c31a9de12cb479b8f969527bf0
Pierre-Louis Launay <lopi@marinlaunay.fr>
2022-04-24 18:40:26 +0200

    Add tests

---
+++ src/Controller/RootController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Lopi\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RootController extends AbstractController
+{
+    /**
+     * @Route("/ping", name="ping", methods={"GET"}, defaults={"_format": "json"})
+     *
+     * @return JsonResponse
+     */
+    public function ping(): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'User tried to ping Gift API');
+
+        return new JsonResponse('Welcome to Gift API');
+    }
+}
