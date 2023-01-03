422fa09a9e86fc34c5777c17726a35a546b580dd
Pierre-Louis Launay <pl.launay@creads.com>
2023-01-03 02:08:22 +0100

    Improve quality code

--- src/State/UserProcessor.php
+++ src/State/UserProcessor.php
@@ -15,15 +15,25 @@ class UserProcessor implements ProcessorInterface
     ) {
     }
 
+    /**
+     * @param mixed        $data
+     * @param Operation    $operation
+     * @param array<mixed> $uriVariables
+     * @param array<mixed> $context
+     *
+     * @return mixed
+     */
     public function process($data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        $data->setPassword(
-            $this->userPasswordHasher->hashPassword(
-                $data,
-                $data->getPlainPassword()
-            )
-        );
-        $data->eraseCredentials();
+        if ($data instanceof User && $data->getPlainPassword() !== null) {
+            $data->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $data,
+                    $data->getPlainPassword()
+                )
+            );
+            $data->eraseCredentials();
+        }
 
         return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
